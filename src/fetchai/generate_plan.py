4a3680fab83d4091693111137f0f79271d84b90c
hchou3 <hchou3@buffalo.edu>
2025-04-23 04:02:53 -0400

    context started

--- src/fetchai/generate_plan.py
+++ src/fetchai/generate_plan.py
@@ -1,2 +1,7 @@
-from uagents import Context, Protocol, Agent
+from uagents import Context, Protocol, Agent, Model
+import json
+import requests
+
+class Message(Model):
+    message : str
 
--- src/fetchai/get_user_details.py
+++ src/fetchai/get_user_details.py
@@ -1,7 +1,9 @@
-from uagents import Context, Protocol, Agent
+from uagents import Context, Protocol, Agent, Model
 
-class GetUserDetails:
-    def get_user_stats(uid: int, age: int, unit: str, weight: float, height: float, activity: str, years: int):
+class Message(Model):
+    message : str
+
+def get_user_stats(uid: int, age: int, unit: str, weight: float, height: float, activity: str, years: int):
         stats = {
             "uid": uid,
             "age": age,
@@ -13,14 +15,23 @@ class GetUserDetails:
         }
 
 
-agent = Agent(name="fit_generator", seed="YOUR NEW PHRASE", port=8000, endpoint=["http://localhost:8000/submit"])   
+
+initial_agent = Agent(name="fit_generator", seed="YOUR NEW PHRASE", port=8000, endpoint=["http://localhost:8000/submit"])   
 generate_fitness_plan = Protocol("Get User Details")
 
+@initial_agent.on_message(model = Message)
+def generate_fitness_plan(message: Message):
+    print(f"Received message: {message.message}")
+
+    response = f"The user's body statistics and history of atheltic activity: {message.message}"
+    return response
+
+context = Context(agent="fit_generator", protocol="Get User Details")
 
 
- 
+
  
 if __name__ == "__main__":
-    agent.include(generate_fitness_plan)
-    agent.run()
+    initial_agent.include(generate_fitness_plan)
+    initial_agent.run()
  
